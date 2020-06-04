31c75a7b70ce0d69355d93458f3b97e9e983d32c
Hnobles12 <Hnobles12@gmail.com>
2020-06-03 22:20:00 -0500

    updates

---
+++ dev/Framework/signal_manager.hpp
@@ -0,0 +1,7 @@
+#ifndef SIGNAL_MANAGER_H
+#define SIGNAL_MANAGER_H
+
+
+
+
+#endif
--- dev/Libraries/C++/RPCC_H/RPCC.hpp
+++ dev/Libraries/C++/RPCC_H/RPCC.hpp
@@ -7,7 +7,7 @@
 //Publisher Class
 class Publisher{
 public:
-    std::string = name, node, workspace_path;
+    std::string name, node, workspace_path;
 
     Publisher(std::string pub_name, std::string node_name, std::string workspace_dir){
         name = pub_name;
@@ -15,7 +15,26 @@ public:
         workspace_path = workspace_dir;
     }
 
-    void publish(auto data){
+    void notify_all(){
+        //send notification to notification pipes
+    }
+    void publish_string(std::string data){
+
+    }
+    void publish_int(int data){
+        //Publish integer 
+
+    }
+    void publish_double(double data){
+
+    }
+    void publish_bool(bool data){
+
+    }
+    void publish_vector_int(std::vector<int> data){
+
+    }
+    void publish_vector_double(std::vector<int> data){
 
     }
 
